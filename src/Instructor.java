51fed2fcf15a32946348b20b247321ea75f64f18
sang29 <sangik.han29@gmail.com>
2021-05-02 16:13:48 -0400

    Updates for IPerson interface and skeleton for student/instructor classes

---
+++ src/Instructor.java
@@ -0,0 +1,81 @@
+
+public class Instructor implements IPerson{
+    private String name;
+    private int id;
+    private int password;
+    private String program;
+    
+    private int[] courses; //list of courses to teach
+    
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setName() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int getId() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setId() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int login(int id, int pw) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int logout(int id, int pw) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void printSchedule() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    //change course info -> maybe get rid of it later?
+    /**
+     * 
+     * @param courseNo
+     * @param cap - set the cap to this new number
+     * @return
+     */
+    public int changeCourseCap(int courseNo, int cap);
+    
+    /**
+     * 
+     * @return 2D matrix with courseNo and studentID for request
+     */
+    public int[][] permRequests();
+    
+    //give permission to the student for given class
+    /**
+     * 
+     * @param studentId
+     * @param courseNo
+     * @return -1 upon failure and 0 upon success
+     * potential failure cases - course already full,
+     * given studentId is not on the request list
+     * courseNo doesn't belong to the instructor
+     */
+    int givePerm(int studentId, int courseNo);
+    
+    
+
+}
---
+++ src/Student.java
@@ -0,0 +1,61 @@
+
+public class Student implements IPerson{
+    private String name;
+    private int id;
+    private int password;
+    private String program;
+    
+    private int[] pastCourses;
+    private int[] cart;
+    
+    
+    
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setName() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int getId() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setId() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int login(int id, int pw) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int logout(int id, int pw) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void printSchedule() {
+        // TODO Auto-generated method stub
+        
+    }
+    
+    int addClass(int classNo);
+    
+    int dropClass(int classNo);
+    
+    int requestPerm(int classNo);
+}
