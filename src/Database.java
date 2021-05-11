533372787a14c0ebae2dd5caf06523ed61d639db
sang29 <sangik.han29@gmail.com>
2021-05-11 14:51:26 -0400

    82% coverage

--- src/Database.java
+++ src/Database.java
@@ -633,26 +633,6 @@ public class Database {
     }
 
     public static void main(String[] args) {
-        Database m = new Database();
-        m.openClient();
-        m.deleteStudentById("sangik1_id");
-        m.deleteStudentById("sangik2_id");
-        m.deleteStudentById("philipp_id");
-        m.deleteInstructorById("eric_id");
-
-        m.pushStudentToDatabase("Sang Ik1", "Han", "ECON", "sangik1_id", "sangik1_pw");
-        m.pushStudentToDatabase("Sang Ik2", "Han", "ECON", "sangik2_id", "sangik2_pw");
-        m.pushStudentToDatabase("Philipp", "Gaissert", "CIT", "philipp_id", "philipp_pw");
-
-        // push prereq for ECON 104 1
-        m.pushPastCourseToStudent("sangik2_id", "ECON", 101);
-        m.pushPastCourseToStudent("sangik2_id", "ECON", 103);
-        m.pushPastCourseToStudent("sangik2_id", "MATH", 104);
-        m.pushPastCourseToStudent("sangik2_id", "MATH", 114);
-
-        // CIS 557 requires permission from Eric Fouh
-        m.pushInstructorToDatabase("Eric", "Fouh", "CIS", "eric_id", "eric_pw");
-        m.closeClient();
 
     }
 
